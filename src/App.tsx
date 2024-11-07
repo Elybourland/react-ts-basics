8d7ccf3835ad1c6e26dfee70db2bb71089dd251d
Elybourland <JEBourland@yahoo.com>
2024-11-06 20:27:15 -0800

    - reusing types, passing functions as values

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import CourseGoal from "./components/CourseGoal.tsx";
 import goalsImg from './assets/goals.jpg';
 import Header from "./components/Header.tsx";
 import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList.tsx";
 
-type CourseGoal = {
+export type CourseGoal = {
   title: string;
   description: string;
   id: number;
-}
+};
 
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
@@ -23,21 +23,17 @@ export default function App() {
     });
   }
 
+  function handleDeleteGoal(id: number) {
+    setGoals(prevGoals => prevGoals.filter((goal) => goal.id !== id));
+  }
+
   return (
     <main>
       <Header image={{src: goalsImg, alt: 'A list of goals'}}>
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <ul>
-        {goals.map((goal) => (
-          <li key={goal.id}>
-            <CourseGoal title={goal.title}>
-              <p>{goal.description}</p>
-            </CourseGoal>
-          </li>
-        ))}
-      </ul>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
 }
